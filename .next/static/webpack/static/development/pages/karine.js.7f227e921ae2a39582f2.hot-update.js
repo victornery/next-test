10b40508bf3de30c90dfb22a6fac182571aba41f
Victor Nery <victornery.t@gmail.com>
2018-11-05 12:28:44 -0300

    First commit

---
+++ .next/static/webpack/static/development/pages/karine.js.7f227e921ae2a39582f2.hot-update.js
@@ -0,0 +1,57 @@
+webpackHotUpdate("static/development/pages/karine.js",{
+
+/***/ "./pages/karine.js":
+/*!*************************!*\
+  !*** ./pages/karine.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/Nery/Desktop/Nery/Estudos/hello-next/pages/karine.js";
+
+
+var MeuTeste = function MeuTeste() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Fragment, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Isso \xE9 um teste! :)"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (MeuTeste);
+    (function (Component, route) {
+      if(!Component) return
+      if (false) {}
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/karine")
+  
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=karine.js.7f227e921ae2a39582f2.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <div>Go to About Page</div>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+)
+
+export default Index
