7b1cba1d270e43fcb7c447c102f49d72b4900239
Victor Nery <victornery.t@gmail.com>
2018-11-13 18:45:06 -0200

    Fetch funcional com o getInitialProps

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f9fd3c46e2e5ff52b5e8";
+/******/ 	var hotCurrentHash = "161cc88efbdc77bf5886";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/post.js
+++ pages/post.js
@@ -1,18 +1,24 @@
 import React, { Fragment } from 'react'
 import { withRouter } from 'next/router'
 import Layout from '../components/MyLayout'
-
-const Content = withRouter((props) => (
-  <Fragment>
-    <h1>{ props.router.query.title }</h1>
-    <p>Esse é o conteúdo do blog, bichão! ;-P</p>
-  </Fragment>
-))
+import fetch from 'isomorphic-unfetch'
 
 const Post = (props) => (
   <Layout>
-    <Content />
+    <h1>{ props.show.name }</h1>
+    <p>{ props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={ props.show.image.medium } />
   </Layout>
 )
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
 
 export default Post
